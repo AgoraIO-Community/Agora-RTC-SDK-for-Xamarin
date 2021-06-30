441da803419f391c08112ddfa171694c241b60d1
Sergey Bokatuk <s.bokatuk@dreamteam-mobile.com>
2021-06-30 14:15:44 +0300

    3.4.4 release

--- Xamarin.Agora.iOS/Xamarin.Agora.Voice.iOS/Properties/AssemblyInfo.cs
+++ Xamarin.Agora.iOS/Xamarin.Agora.Voice.iOS/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.4.2")]
+[assembly: AssemblyVersion("3.4.4")]
